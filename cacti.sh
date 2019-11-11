028c748d3bc3d78c8d435e2c64ed3395466c81be
kuroinvidia <57119803+kuroinvidia@users.noreply.github.com>
2019-11-11 19:35:49 +0700

    Update cacti.sh

--- cacti.sh
+++ cacti.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 apt update
 apt install mariadb-server mariadb-client -y
 mysql_secure_installation 
-wget https://raw.githubusercontent.com/kuroinvidia/linux/master/50-server.cnf
+wget http://github.com/kuroinvidia/linux/raw/master/50-server.cnf
 cp 50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
 systemctl restart mariadb
 apt install apache2 php php-mysql php-snmp -y
 apt install cacti snmpd -y
-echo -e " \n \n admin \n admin \n"
+echo -e "\n \n admin \n admin \n"
 mysql -u root -p -D cacti -e "update user_auth set password=md5('admin') where username='admin';"
 
 
