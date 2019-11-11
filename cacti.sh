c631fd5d7bcfd8a127096c104d7a40ce0abfaa3c
kuroinvidia <57119803+kuroinvidia@users.noreply.github.com>
2019-11-11 11:03:07 +0700

    Add files via upload

---
+++ cacti.sh
@@ -0,0 +1,16 @@
+apt update
+apt install mariadb-server mariadb-client -y
+mysql_secure_installation 
+wget https://raw.githubusercontent.com/kuroinvidia/linux/master/50-server.cnf
+cp 50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
+systemctl restart mariadb
+apt install apache2 php php-mysql php-snmp -y
+apt install cacti snmpd -y
+echo -e " \n \n admin \n admin \n"
+mysql -u root -p -D cacti -e "update user_auth set password=md5('admin') where username='admin';"
+
+
+
+
+
+
