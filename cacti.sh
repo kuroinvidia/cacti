c6b7ebce1816f34b55932fd0da993cadc18fc3c8
kuroinvidia <57119803+kuroinvidia@users.noreply.github.com>
2019-11-18 06:48:35 +0700

    Update cacti.sh

--- cacti.sh
+++ cacti.sh
@@ -2,7 +2,6 @@
 apt update
 apt install dos2unix mariadb-server mariadb-client -y
 mysql_secure_installation
-wget https://github.com/kuroinvidia/linux/raw/master/50-server.cnf
 cp 50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
 systemctl restart mariadb
 apt install apache2 php php-mysql php-snmp -y
