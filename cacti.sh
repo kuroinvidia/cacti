00f87a699018fce1b3ce2177255552c6ec571af0
kuroinvidia <57119803+kuroinvidia@users.noreply.github.com>
2019-11-23 12:40:39 +0700

    Update cacti.sh

--- cacti.sh
+++ cacti.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 apt update
-apt install dos2unix mariadb-server mariadb-client -y
+apt install mariadb-server mariadb-client -y
 mysql_secure_installation
 cp 50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
 systemctl restart mariadb
 apt install apache2 php php-mysql php-snmp -y
 apt install cacti snmpd -y
+cp snmpd.conf /etc/snmp/snmpd.conf
+
+$ip
+$rocommunity
+
+echo 'Your Server IP!!!'
+read -p 'IP Address Cacti : ' ip
+sed -i "s+192.168.55.3+$ip+g" /etc/snmp/snmpd.conf
+
+echo 'Your Rocommunity!!'
+read -p 'Rocommunity : ' rocommunity
+sed -i "s+alice+$rocommunity" /etc/snmp/snmpd.conf
+
+#admin is password you can changes it
 echo -e "\n \n admin \n admin \n"
 mysql -u root -p -D cacti -e "update user_auth set password=md5('admin') where username='admin';"
-
-
